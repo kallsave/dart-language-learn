16faffe8fba246fc38aaa5bfbe9608a66c809860
kallsave <415034609@qq.com>
2020-07-21 23:55:59 +0800

    init

---
+++ defined/index.dart
@@ -0,0 +1,18 @@
+void main() {
+  // 定义常量,有final,const两种
+  // 但是final定义的常量是运行时常量
+  // 而const是编译时常量
+  final time = new DateTime.now();
+  const PI = 3.14;
+
+  // dart语言是强类型语言
+  // 但是可以用var去推断
+  var str = "ss";
+
+  // 字符串
+  String name = "name";
+
+  // 整型
+  int count = 100;
+
+}
